4f766bad5c56b6b44441d59ced0ece50a8121c02
Guillermo Pineda <pinedag395@gmail.com>
2018-11-06 17:15:09 -0800

    Added more efficient method for finding parity

--- primitives/parity.py
+++ primitives/parity.py
@@ -1,9 +1,30 @@
 # Compute Parity of word
+
 # Bruteforce Method
+# Time Complexity: O(n) where n is word size
+# def parity(x):
+#     result = 0
+#     #terately tests value of each bit, while keeping count of 1s seen so far
+#     while x:
+#         result ^= x & 1
+#         x >>= 1
+#     return result
+
+# Faster Method
+# Time Complexity: O(k) where is number of bits
 def parity(x):
     result = 0
     while x:
-        result ^= x & 1
-        x >>= 1
+        result ^= 1
+        x &= x - 1
     return result
-    
+
+# Parity Method for large numbers
+def parity(x):
+    x ^= x >> 32
+    x ^= x >> 16
+    x ^= x >> 8
+    x ^= x >> 4
+    x ^= x >> 2
+    x ^= x >> 1
+    return x & 0x1
