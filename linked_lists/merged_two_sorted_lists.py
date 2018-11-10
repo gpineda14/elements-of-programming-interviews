18efb85acd5c37b6d899863b05abc44a8f278c16
Guillermo Pineda <pinedag395@gmail.com>
2018-11-09 16:54:55 -0800

    Added changes to merge_two_sorted_lists

--- linked_lists/merged_two_sorted_lists.py
+++ linked_lists/merged_two_sorted_lists.py
@@ -5,12 +5,11 @@ def merge_two_sorted_lists(l1, l2):
     while l1 and l2:
         if l1.data < l2.data:
             curr.next = l1
-            l1.next
+            l1 = l1.next
         else:
             curr.next = l2.data
-            l2.next
+            l2 = l2.next
         curr = curr.next
 
     curr.next = l1 or l2
     return list.next
-    
